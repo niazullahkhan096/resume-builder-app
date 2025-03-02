6c5ddc7ba73536e9a3de73c1bcf1680d86b407e8
Niaz <59473091+niazullahkhan096@users.noreply.github.com>
2025-03-02 12:52:04 +0500

    new templates

--- server-app/src/controllers/resume.controller.ts
+++ server-app/src/controllers/resume.controller.ts
@@ -11,7 +11,7 @@ class ResumeController {
   public async generateResume(req: Request, res: Response): Promise<void> {
     try {
       const resumeData = req.body;
-      const latexDocument = await this.latexService.generateLatex('alta', resumeData);
+      const latexDocument = await this.latexService.generateLatex('mamyer', resumeData);
 
       res.status(200).send(
         {
